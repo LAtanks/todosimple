efac02faef1aba0ede7bcee926557e9228f2cd3d
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2025-01-08 22:28:31 -0300

    First commit

---
+++ src/test/java/me/learning/TodoSimple/TodoSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.learning.TodoSimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
