92c187d3f4ef1bc07818945a279ed75253f68589
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2025-01-09 20:19:44 -0300

    Create User model

--- src/main/java/me/learning/TodoSimple/TodoSimpleApplication.java
+++ src/main/java/me/learning/TodoSimple/TodoSimpleApplication.java
@@ -1,5 +1,6 @@
 package me.learning.TodoSimple;
 
+import me.learning.TodoSimple.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class TodoSimpleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodoSimpleApplication.class, args);
 	}
-
 }
---
+++ src/main/java/me/learning/TodoSimple/models/User.java
@@ -0,0 +1,78 @@
+package me.learning.TodoSimple.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    public interface CreateUser{}
+    public interface UpdateUser{}
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class},min = 2, max = 100)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password", length = 60, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class},min = 8, max = 60)
+    private String password;
+
+    //private List<Task> tasks = new ArrayList<Task>();
+
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this)
+            return true;
+        if(o == null)
+            return false;
+        if(!(o instanceof  User))
+            return false;
+        User user = (User) o;
+
+        if(this.id == null){
+            if(user.id != null)
+                return false;
+            else if(!this.id.equals(user.id))
+                return false;
+
+        }
+
+        return Objects.equals(this.id, user.id) && Objects.equals(this.username, user.username) && Objects.equals(this.password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+}
