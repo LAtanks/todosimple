a2cbc4d4c7c6fbb2904ff9247b2ecde509a5f6d6
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2025-01-29 11:20:36 -0300

    Add custom bad credentials exception

--- src/main/java/me/learning/TodoSimple/controllers/AuthController.java
+++ src/main/java/me/learning/TodoSimple/controllers/AuthController.java
@@ -5,6 +5,7 @@ import me.learning.TodoSimple.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,15 +26,20 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody @Valid User data) {
-
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        data.getUsername(),
-                        data.getPassword()
-                )
-        );
-
-        return ResponseEntity.ok().body(Map.of("token", "Bearer "+jwtUtil.generateToken(data)));
-
+        try {
+            User user = new User();
+            user.setUsername(data.getUsername());
+            user.setPassword(data.getPassword());
+            user.setId(data.getId());
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getUsername(),
+                            user.getPassword()
+                    )
+            );
+            return ResponseEntity.ok().body(Map.of("token", "Bearer "+jwtUtil.generateToken(data)));
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Usuário ou senha inválidos");
+        }
     }
 }
